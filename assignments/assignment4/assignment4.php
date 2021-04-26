aaaa9860e115499ce57b152649dad54ab82e3d5e
Ivan Lei <ilei0893@gmail.com>
2021-04-26 12:34:29 -0400

    Finished part 1

--- assignments/assignment4/assignment4.php
+++ assignments/assignment4/assignment4.php
@@ -36,6 +36,50 @@
     </div>
   </nav>
 
+  <div class="part1">
+    <h1>Part 1: Warm Up</h1>
+    <h2>
+      1. What is a database? Why would we implement it in our system/website?
+    </h2>
+    <p>
+      A database is a place to store data. We should implement these in systems/websites if we want to store any kind of
+      data such as accounts or user info.
+    </p>
+    <h2>
+      2. What is the advantage of using a relational database management system?
+    </h2>
+    <p>
+      Using a RDBMS is advanatagous because it has a simple design, has easy access to data, is flexible, and is
+      maintainable.
+    </p>
+    <h2>
+      3. List the different types of relations in a database.
+    </h2>
+    <p>
+    <ul>
+      <li>
+        One-to-One
+      </li>
+      <li>
+        One-to-Many
+      </li>
+      <li>
+        Many-to-Many
+      </li>
+    </ul>
+    </p>
+    <h2>
+      4. Explain the difference between key, primary key and foreign key.
+    </h2>
+    <p>
+      A key is synonymous to an index, it identifies a record. A primary key is a specific type of unique key constraint
+      that ensures data in a column is unique. <br> The difference between a primary key and a foreign key is that a
+      foreign
+      key creates a relation between two tables and often refers to the primary key in another table.
+    </p>
+  </div>
+  <hr>
+
 
 
   <!-- Optional JavaScript -->
